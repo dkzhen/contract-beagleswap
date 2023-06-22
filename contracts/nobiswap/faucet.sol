b9a9540136d9128514f3708d36b7e2e7a6706e1d
Dani Kurniawan <47886545+dkzhen@users.noreply.github.com>
2023-06-22 23:34:15 +0700

    Update faucet.sol

--- contracts/nobiswap/faucet.sol
+++ contracts/nobiswap/faucet.sol
@@ -5,6 +5,9 @@ interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
     function claimTokens() external;
+    function withdraw() external;
+
+
 }
 
 contract Nobyswap {
